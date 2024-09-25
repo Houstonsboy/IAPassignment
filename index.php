89a57ed253f68c014e5671b0f37197ee53c00b57
Kristein-Gichuhi <kristein.mwaura@strathmore.edu>
2024-09-25 13:55:47 +0300

    my first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+require "load.php";
+$ObjLayouts->heading();
+$ObjNav->navBar();
+// $ObjHeadings->main_banner();
+// $ObjCont->main_content();
+// $ObjCont->side_bar();
+$ObjLayouts->footer();
---
+++ load.php
@@ -0,0 +1,33 @@
+<?php
+
+// require "includes/constants.php";
+require "database/dbConnection.php";
+
+// Class Auto Load 
+
+function classAutoLoad($classname){
+
+    $directories = [ "layouts","database", "forms", "navigation"];
+
+    foreach($directories AS $dir){
+        $filename = dirname(__FILE__) . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR . $classname . ".php";
+        if(file_exists($filename) AND is_readable($filename)){
+            require_once $filename;
+        }
+    }
+}
+
+spl_autoload_register('classAutoLoad');
+
+// Create instances of all classes
+    $ObjLayouts = new layouts();
+    $ObjNav = new NavBar();
+    // $ObjCont = new contents();
+    // $ObjForm = new user_forms();
+    // $conn = new dbConnection(DBTYPE, HOSTNAME, DBPORT, HOSTUSER, HOSTPASS, DBNAME);
+
+
+// Create process instances
+
+    // $ObjAuth = new auth();
+    // $ObjAuth->signup($conn);
