d86fa9830a6db4f651b144b7d839e90c8d05adbd
Kristein-Gichuhi <kristein.mwaura@strathmore.edu>
2024-10-01 23:17:35 +0300

    centered the lookkup form

--- forms/lookup.php
+++ forms/lookup.php
@@ -2,14 +2,14 @@
 class lookup {
     public function lookup_form() {
         ?>
-       <nav class="navbar bg-body-tertiary">
-  <div class="container-fluid">
-    <form class="d-flex" role="search">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
+        <nav class="navbar bg-body-tertiary">
+            <div class="container-fluid d-flex justify-content-center">
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
         <?php
     }
 }
