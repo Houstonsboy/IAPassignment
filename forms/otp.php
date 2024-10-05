8c5fb4b8079b95e9d4acc1e07a01eaf6bcea617b
Kristein-Gichuhi <kristein.mwaura@strathmore.edu>
2024-10-05 20:17:24 +0300

    styled the otp form

--- forms/otp.php
+++ forms/otp.php
@@ -3,20 +3,22 @@
 class otp {
     public function opt_form($errorMessage = null) {
         ?>
+        <h1 style="text-align:center">An otp code has been sent to your email.</h1>
         <nav class="navbar bg-body-tertiary">
-            <div class="container-fluid d-flex justify-content-center">
-                <?php if (!empty($errorMessage)): ?>
-                    <div class="alert alert-danger">
-                        <?php echo $errorMessage; ?>
-                    </div>
-                <?php endif; ?>
-                <form action="" method="POST" class="d-flex" enctype="multipart/form-data">
-                    <label for="otpcode">OTP Code</label>
-                    <input class="form-control me-2" type="text" name="otpcode" placeholder="Enter OTP" required>
-                    <button class="btn btn-outline-success" type="submit" name="otpverify">Verify</button>
-                </form>
+    <div class="container-fluid d-flex justify-content-center align-items-center vh-100">
+        <?php if (!empty($errorMessage)): ?>
+            <div class="alert alert-danger">
+                <?php echo $errorMessage; ?>
             </div>
-        </nav>
+        <?php endif; ?>
+        <form action="" method="POST" class="d-flex flex-column align-items-center" enctype="multipart/form-data">
+            <label for="otpcode" class="mb-2">OTP Code</label>
+            <input class="form-control mb-2" type="text" name="otpcode" placeholder="Enter OTP" required>
+            <button class="btn btn-outline-success" type="submit" name="otpverify">Verify</button>
+        </form>
+    </div>
+</nav>
+
         <?php
     }
 }
